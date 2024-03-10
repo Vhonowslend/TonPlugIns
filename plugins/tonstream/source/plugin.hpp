24eb975d2b31f48e001ad19ecd23c79d498f3206
Michael Fabian 'Xaymar' Dirks <info@xaymar.com>
2024-03-10 18:45:31 +0100

    Split plugins into submodules

--- plugins/tonstream/source/plugin.hpp
+++
@@ -1,9 +0,0 @@
-// Copyright 2023 Michael Fabian 'Xaymar' Dirks < info @xaymar.com>
-
-#pragma once
-#include <version.hpp>
-#include <core.hpp>
-
-#include <warning-disable.hpp>
-#include <public.sdk/source/main/moduleinit.h>
-#include <warning-enable.hpp>
